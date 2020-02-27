e001942436eba1fe8154aed2cbcfe12674ff7568
Him188 <Him188@mamoe.net>
2020-02-27 14:06:35 +0800

    Add missing `mirai-japt` version

--- mirai-demo-java/build.gradle.kts
+++ mirai-demo-java/build.gradle.kts
@@ -6,8 +6,10 @@ plugins {
 val kotlinVersion: String by rootProject.ext
 
 val miraiCoreVersion: String by rootProject.ext
+val miraiJaptVersion: String by rootProject.ext
 
 dependencies {
     implementation("net.mamoe:mirai-core-jvm:$miraiCoreVersion")
     implementation("net.mamoe:mirai-core-qqandroid-jvm:$miraiCoreVersion")
+    implementation("net.mamoe:mirai-japt:$miraiJaptVersion")
 }
