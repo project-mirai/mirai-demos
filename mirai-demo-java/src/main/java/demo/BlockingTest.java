a13848a56373389ad1301165b65ba5da2cfe0dd5
Him188 <Him188@mamoe.net>
2020-03-06 10:05:09 +0800

    Update demos

--- mirai-demo-java/src/main/java/demo/BlockingTest.java
+++ mirai-demo-java/src/main/java/demo/BlockingTest.java
@@ -5,13 +5,10 @@ import kotlinx.coroutines.Job;
 import net.mamoe.mirai.Bot;
 import net.mamoe.mirai.BotFactoryJvm;
 import net.mamoe.mirai.contact.Contact;
-import net.mamoe.mirai.contact.Member;
 import net.mamoe.mirai.japt.Events;
 import net.mamoe.mirai.message.GroupMessage;
 import net.mamoe.mirai.message.MessageReceipt;
-import net.mamoe.mirai.message.data.At;
-import net.mamoe.mirai.message.data.AtAll;
-import net.mamoe.mirai.message.data.Image;
+import net.mamoe.mirai.message.data.*;
 import net.mamoe.mirai.utils.BotConfiguration;
 import net.mamoe.mirai.utils.SystemDeviceInfoKt;
 
@@ -44,51 +41,60 @@ class BlockingTest {
         });
 
         Events.subscribeAlways(GroupMessage.class, (GroupMessage event) -> {
-            final Member sender = event.getSender();
 
-            // 异步发送消息:
+            if (event.getMessage().contains("reply")) {
+                // 引用回复
+                final QuoteReplyToSend quote = MessageUtils.quote(event.getMessage(), event.getSender());
+                event.getGroup().sendMessage(quote.plus("引用回复"));
 
-            final Future<MessageReceipt<Contact>> future = sender.sendMessageAsync("Async send"); // 异步发送
-            try {
-                future.get();
-            } catch (InterruptedException | ExecutionException e) {
-                e.printStackTrace();
+            } else if (event.getMessage().contains("at")) {
+                // at
+                event.getGroup().sendMessage(new At(event.getSender()));
+
+            } else if (event.getMessage().contains("permission")) {
+                // 成员权限
+                event.getGroup().sendMessage(event.getPermission().toString());
+
+            } else if (event.getMessage().contains("mixed")) {
+                // 复合消息, 通过 .plus 连接两个消息
+                event.getGroup().sendMessage(
+                        Image.fromId("{01E9451B-70ED-EAE3-B37C-101F1EEBF5B5}.png") // 演示图片, 可能已过期
+                                .plus("Hello") // 文本消息
+                                .plus(new At(event.getSender())) // at 群成员
+                                .plus(AtAll.INSTANCE) // at 全体成员
+                );
+
+            } else if (event.getMessage().contains("recall1")) {
+                event.getGroup().sendMessage("你看不到这条消息").recall();
+                // 发送消息马上就撤回. 因速度太快, 客户端将看不到这个消息.
+
+            } else if (event.getMessage().contains("recall2")) {
+                final Job job = event.getGroup().sendMessage("3秒后撤回").recallIn(3000);
+
+                // job.cancel(new CancellationException()); // 可取消这个任务
+
+            } else if (event.getMessage().contains("上传图片")) {
+                File file = new File("myImage.jpg");
+                if (file.exists()) {
+                    final Image image = event.getGroup().uploadImage(new File("myImage.jpg"));
+                    // 上传一个图片并得到 Image 类型的 Message
+
+                    final String imageId = image.getImageId(); // 可以拿到 ID
+                    final Image fromId = Image.fromId(imageId); // ID 转换得到 Image
+
+                    event.getGroup().sendMessage(image); // 发送图片
+                }
+
+            } else if (event.getMessage().contains("friend")) {
+                final Future<MessageReceipt<Contact>> future = event.getSender().sendMessageAsync("Async send"); // 异步发送
+                try {
+                    future.get();
+                } catch (InterruptedException | ExecutionException e) {
+                    e.printStackTrace();
+                }
             }
-
-
-            // 阻塞发送消息, 撤回:
-            final Job job = sender.sendMessage("Hello World!") // 阻塞发送消息
-                    .recallIn(3000); // 启动一个异步任务, 三秒后撤回这条消息
-
-            // job.cancel(new CancellationException()); // 可以取消这个任务
-
-            sender.sendMessage("你看不到这条消息").recall(); // 发送消息马上就撤回. 因速度太快, 客户端将看不到这个消息.
-
-
-            File file = new File("myImage.jpg");
-            if (file.exists()) {
-                final Image image = sender.uploadImage(new File("myImage.jpg"));
-                // 上传一个图片并得到 Image 类型的 Message
-
-                final String imageId = image.getImageId(); // 可以拿到 ID
-                final Image fromId = Image.fromId(imageId); // ID 转换得到 Image
-
-                event.getGroup().sendMessage(image); // 发送图片
-            }
-
-            event.quote(event.getMessage());
-
-            event.getPermission(); // 发消息的群员的权限
-
-            // 复合消息, 通过 .plus 连接两个消息
-            event.getGroup().sendMessage(
-                    Image.fromId("{01E9451B-70ED-EAE3-B37C-101F1EEBF5B5}.png") // 演示图片, 可能已过期
-                            .plus("Hello") // 文本消息
-                            .plus(new At(event.getSender())) // at 群成员
-                            .plus(AtAll.INSTANCE) // at 全体成员
-            );
         });
 
-        bot.join();
+        bot.join(); // 阻塞当前线程直到 bot 离线
     }
 }
