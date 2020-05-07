73565c90ac990ac8442ba7ff0a5525d3baf03fb3
sandtechnology <20417547+sandtechnology@users.noreply.github.com>
2020-05-07 10:16:26 +0800

    Update to 1.0.0-RC (#14)
    * Update to the latest version
    
    * Fix build error
    
    * update to the latest version
    
    * update to the latest version
    
    * Suitable to the latest version(thanks to @Him188)
    
    * Merge
    
    * Update to the latest version (#4)
    
    * Update to the latest version
    
    * Fix build error
    
    * Replace with constructing ContextImpl instance and change to the latest version
    
    * Doing update again
    
    * Fix android target build
    
    * Changing toString method
    
    * Tweaking toString method, add example for kotlin return value
    
    * Update to 0.39.4 and add some temp message examples
    
    * Update to 1.0.0-RC
    
    * 火 华为 火
    
    * Commented mirror repositories
    
    * Strange....try it again

--- mirai-demo-java/src/main/java/demo/BlockingTest.java
+++ mirai-demo-java/src/main/java/demo/BlockingTest.java
@@ -6,7 +6,7 @@ import net.mamoe.mirai.Bot;
 import net.mamoe.mirai.BotFactoryJvm;
 import net.mamoe.mirai.contact.Contact;
 import net.mamoe.mirai.japt.Events;
-import net.mamoe.mirai.message.GroupMessage;
+import net.mamoe.mirai.message.GroupMessageEvent;
 import net.mamoe.mirai.message.MessageReceipt;
 import net.mamoe.mirai.message.data.*;
 import net.mamoe.mirai.utils.BotConfiguration;
@@ -23,6 +23,7 @@ class BlockingTest {
         // 使用自定义的配置
         final Bot bot = BotFactoryJvm.newBot(123, "", new BotConfiguration() {
             {
+                //保存设备信息到文件
                 setDeviceInfo(context ->
                         SystemDeviceInfoKt.loadAsDeviceInfo(new File("deviceInfo.json"), context)
                 );
@@ -37,26 +38,30 @@ class BlockingTest {
 
         bot.login();
 
+        //输出好友
         bot.getFriends().forEach(friend -> System.out.println(friend.getId() + ":" + friend.getNick()));
 
 
-        Events.subscribeAlways(GroupMessage.class, (GroupMessage event) -> {
+        //此处订阅的event为LOCKED模式，不需要考虑并发问题
+        //如需要并发处理，请使用kotlin订阅事件
+        Events.subscribeAlways(GroupMessageEvent.class, event -> {
 
 
-            if (toString(event.getMessage()).contains("reply")) {
+            String msgString = toString(event.getMessage());
+            if (msgString.contains("reply")) {
                 // 引用回复
                 final QuoteReply quote = MessageUtils.quote(event.getMessage());
                 event.getGroup().sendMessage(quote.plus("引用回复"));
 
-            } else if (toString(event.getMessage()).contains("at")) {
+            } else if (msgString.contains("at")) {
                 // at
                 event.getGroup().sendMessage(new At(event.getSender()));
 
-            } else if (toString(event.getMessage()).contains("permission")) {
+            } else if (msgString.contains("permission")) {
                 // 成员权限
                 event.getGroup().sendMessage(event.getPermission().toString());
 
-            } else if (toString(event.getMessage()).contains("mixed")) {
+            } else if (msgString.contains("mixed")) {
                 // 复合消息, 通过 .plus 连接两个消息
                 event.getGroup().sendMessage(
                         MessageUtils.newImage("{01E9451B-70ED-EAE3-B37C-101F1EEBF5B5}.png") // 演示图片, 可能已过期
@@ -64,17 +69,16 @@ class BlockingTest {
                                 .plus(new At(event.getSender())) // at 群成员
                                 .plus(AtAll.INSTANCE) // at 全体成员
                 );
-
-            } else if (toString(event.getMessage()).contains("recall1")) {
+            } else if (msgString.contains("recall1")) {
                 event.getGroup().sendMessage("你看不到这条消息").recall();
                 // 发送消息马上就撤回. 因速度太快, 客户端将看不到这个消息.
 
-            } else if (toString(event.getMessage()).contains("recall2")) {
+            } else if (msgString.contains("recall2")) {
                 final Job job = event.getGroup().sendMessage("3秒后撤回").recallIn(3000);
 
                 job.cancel(new CancellationException()); // 可取消这个任务
 
-            } else if (toString(event.getMessage()).contains("上传图片")) {
+            } else if (msgString.contains("上传图片")) {
                 File file = new File("myImage.jpg");
                 if (file.exists()) {
                     final Image image = event.getGroup().uploadImage(new File("myImage.jpg"));
@@ -86,7 +90,7 @@ class BlockingTest {
                     event.getGroup().sendMessage(image); // 发送图片
                 }
 
-            } else if (toString(event.getMessage()).contains("friend")) {
+            } else if (msgString.contains("friend")) {
                 final Future<MessageReceipt<Contact>> future = event.getSender().sendMessageAsync("Async send"); // 异步发送
                 try {
                     future.get();
@@ -94,7 +98,7 @@ class BlockingTest {
                     e.printStackTrace();
                 }
                 //将图片转换为图片ID
-            } else if (toString(event.getMessage()).startsWith("convert")) {
+            } else if (msgString.startsWith("convert")) {
                 StringBuilder stringBuilder = new StringBuilder("结果：\n");
                 event.getMessage().forEachContent(msg ->
                         {
@@ -106,6 +110,12 @@ class BlockingTest {
                         }
                 );
                 event.getGroup().sendMessage(stringBuilder.toString());
+            } else if (msgString.equals("mute")) {
+                //禁言
+                event.getSender().mute(100);
+            } else if (msgString.equals("muteAll")) {
+                //全体禁言
+                event.getGroup().getSettings().setMuteAll(true);
             }
         });
 
